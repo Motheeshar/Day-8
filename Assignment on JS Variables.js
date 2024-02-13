1b4b67822cbc6a4f7296949322459e187429c00e
A R Motheesh <ar.motheesh@gmail.com>
2024-02-13 19:05:53 +0530

    Add files via upload

---
+++ Assignment on JS Variables.js
@@ -0,0 +1,32 @@
+// Do not edit the below 2 lines of code
+const nameContainer = document.getElementById("name-container");
+const ageContainer = document.getElementById("age-container");
+
+// Challenge 1: Building Profile
+const myName = "Motheesh A R"; 
+nameContainer.innerHTML = myName;
+
+// Challenge 2: Create a variable with the identifier "myAge"
+var myAge = 23; 
+ageContainer.innerHTML = myAge;
+
+// Challenge 3: Debugging challenge
+const firstQuestion =
+  "This is the first debugging question. If you fix this question, you will be able to see this text on your console";
+document.write(firstQuestion + "<br>");
+
+const secondQuestion =
+  "This is the second debugging question, if you fix this question you will be able to see this text on your console";
+document.write(secondQuestion + "<br>");
+
+const $thirdQuestion =
+  "This is the third debugging question, if you fix this question you will be able to see this text on your console";
+document.write($thirdQuestion + "<br>");
+
+const _fourthQuestion =
+  "This is the fourth debugging question, if you fix this question you will be able to see this text on your console";
+document.write(_fourthQuestion + "<br>");
+
+const fifthQuestion =
+  "This is the fifth debugging question, if you fix this question you will be able to see this text on your console";
+document.write(fifthQuestion + "<br>");
